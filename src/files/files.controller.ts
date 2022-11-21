d600c998d3e1e5100456a80b621e8014845fbbb8
rafiki <71207415+rafikidota@users.noreply.github.com>
2022-11-21 04:34:49 -0500

    Upload file without save it
    -UseInterceptors
    -FileInterceptor
    -Express.Multer.File
    -UploadedFile

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,34 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors, BadRequestException } from '@nestjs/common';
 import { FilesService } from './files.service';
-import { CreateFileDto } from './dto/create-file.dto';
-import { UpdateFileDto } from './dto/update-file.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/file-filter.helper';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(private readonly filesService: FilesService) { }
 
-  @Post()
-  create(@Body() createFileDto: CreateFileDto) {
-    return this.filesService.create(createFileDto);
+  @Post('product')
+  @UseInterceptors(FileInterceptor('file', { fileFilter: fileFilter }))
+  uploadFile(
+    @UploadedFile() file: Express.Multer.File
+  ) {
+    if (!file) {
+      throw new BadRequestException('Make sure that the file is an image');
+    }
+    return {
+      fileName: file.originalname
+    };
   }
 
-  @Get()
-  findAll() {
-    return this.filesService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.filesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFileDto: UpdateFileDto) {
-    return this.filesService.update(+id, updateFileDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.filesService.remove(+id);
-  }
 }

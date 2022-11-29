f707f4b6872b6b35bfa488a31e4134bf1a0a114d
rafiki <71207415+rafikidota@users.noreply.github.com>
2022-11-28 23:09:54 -0500

    Create common service

---
+++ src/common/common.service.ts
@@ -0,0 +1,13 @@
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+
+@Injectable()
+export class CommonService {
+    private readonly logger = new Logger('ProductService'); 
+    private handleExceptions(error: any) {
+        if (error.code === '23505') {
+          throw new BadRequestException(error.detail);
+        }
+        this.logger.error(error);
+        throw new InternalServerErrorException(`Unexpected error, check server logs`)
+      }
+}

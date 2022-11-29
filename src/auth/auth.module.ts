dfbc5d05ed98bd6f5affc7e885bb03b20b4b26a7
rafiki <71207415+rafikidota@users.noreply.github.com>
2022-11-29 00:36:36 -0500

    Add jwt secret key

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,14 +4,23 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { CommonModule } from '../common/common.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
     TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: '',
+      signOptions: {
+        expiresIn: '24h'
+      }
+    }),
     CommonModule
   ],
-  exports:[TypeOrmModule]
+  exports: [TypeOrmModule]
 })
-export class AuthModule {}
+export class AuthModule { }

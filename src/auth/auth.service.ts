81869567939470742fe22fa1d38944f8625a2988
rafiki <71207415+rafikidota@users.noreply.github.com>
2022-11-28 23:36:47 -0500

    Add bcrypt for hashing

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,11 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { CommonService } from '../common/common.service';
 import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { CommonService } from '../common/common.service';
 
 @Injectable()
 export class AuthService {
@@ -13,17 +14,21 @@ export class AuthService {
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
     private readonly commonService: CommonService
-    
+
   ) { }
 
   async create(createAuthDto: CreateUserDto) {
 
     try {
-      const user = this.userRepository.create(createAuthDto);
+      const { password, ...userData } = createAuthDto;
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync(password, 10)
+      });
       await this.userRepository.save(user);
       return user;
     } catch (error) {
-      this.commonService.handleExceptions(error,'AuthService');
+      this.commonService.handleExceptions(error, 'AuthService');
     }
   }
 

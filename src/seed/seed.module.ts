590f6623fd26340202abe0dae0ac0c5a3b0340f5
rafiki <71207415+rafikidota@users.noreply.github.com>
2022-12-01 03:16:56 -0500

    Update seed.module.ts

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
-import { ProductModule } from 'src/product/product.module';
 import { AuthModule } from '../auth/auth.module';
+import { ProductModule } from '../product/product.module';
 
 @Module({
   controllers: [SeedController],

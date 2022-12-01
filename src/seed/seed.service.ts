7245e435eea6fd148fc09c6a51a7c3cf3e318f83
rafiki <71207415+rafikidota@users.noreply.github.com>
2022-12-01 03:43:10 -0500

    Add users to seed

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -3,25 +3,51 @@ import { ProductService } from '../product/product.service';
 import { initialData } from './data/seed-data';
 import { Auth } from '../auth/decorators/auth.decorator';
 import { ValidRoles } from '../auth/interfaces/valid-roles';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../auth/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class SeedService {
 
   constructor(
-    private readonly productService: ProductService
+    private readonly productService: ProductService,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
   ) { }
-  @Auth(ValidRoles.superUser)
+
   async runSeed() {
-    this.insertNewProducts();
+    this.deleteTables();
+    const adminUser = await this.insertUsers();
+    this.insertNewProducts(adminUser);
     return `Seed executed`;
   }
 
-  private async insertNewProducts() {
+  private async deleteTables() {
+    this.productService.deleteAll();
+    const queryBuilder = this.userRepository.createQueryBuilder();
+    await queryBuilder
+      .delete()
+      .where({})
+      .execute()
+  }
+
+  private async insertUsers() {
+    const seedUsers = initialData.users;
+    const users: User[] = [];
+    seedUsers.forEach(user => {
+      users.push(this.userRepository.create(user))
+    });
+    await this.userRepository.save(users);
+    return users[0];
+  }
+
+  private async insertNewProducts(user: User) {
     await this.productService.deleteAll();
     const products = initialData.products;
     const insertPromises = [];
     products.forEach(product => {
-      insertPromises.push(this.productService.create(product));
+      insertPromises.push(this.productService.create(product, user));
     });
     await Promise.all(insertPromises);
     return true;

d188e5a8951af2d93dd96b4ff3645c7fb1567c8a
rafiki <71207415+rafikidota@users.noreply.github.com>
2022-12-05 01:07:53 -0500

    Set title and description  API

--- src/main.ts
+++ src/main.ts
@@ -16,8 +16,8 @@ async function bootstrap() {
   );
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Teslo RESTful API')
+    .setDescription('Teslo Shop Endpoints')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);

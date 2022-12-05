a19701ed75792f23038ce08d8ceea04eb71cf5cb
rafiki <71207415+rafikidota@users.noreply.github.com>
2022-12-05 00:59:48 -0500

    Add Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('Bootstrap');
   app.setGlobalPrefix('api');
@@ -12,6 +14,15 @@ async function bootstrap() {
       forbidNonWhitelisted: true
     })
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.PORT;
   await app.listen(port);
   logger.log(`App running on port ${process.env.PORT} :)`);

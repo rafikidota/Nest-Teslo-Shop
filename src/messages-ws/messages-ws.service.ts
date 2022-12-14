d10566a3a83d70a6b3c01252baaf5e9a7fffb82b
rafiki <71207415+rafikidota@users.noreply.github.com>
2022-12-14 02:52:36 -0500

    Register, remove and get connectedClients

--- src/messages-ws/messages-ws.service.ts
+++ src/messages-ws/messages-ws.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { Socket } from 'socket.io';
+
+interface ConnectedClient {
+    [id: string]: Socket
+}
 
 @Injectable()
-export class MessagesWsService {}
+export class MessagesWsService {
+    private connectedClients = {};
+
+    register(client: Socket) {
+        this.connectedClients[client.id] = client;
+    }
+    remove(clientID: string) {
+        delete this.connectedClients[clientID];
+    }
+    getConnectedClients(): number {
+        return Object.keys(this.connectedClients).length;
+    }
+}

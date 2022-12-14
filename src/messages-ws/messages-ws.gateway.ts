a7f7821009d52e0ab464e724b407a7276be85a0c
rafiki <71207415+rafikidota@users.noreply.github.com>
2022-12-14 03:19:25 -0500

    Emit client connection or client disconnection

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,17 +1,20 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 import { MessagesWsService } from './messages-ws.service';
 
 @WebSocketGateway({ cors: true})
-// @WebSocketGateway({ cors: true, namespace: '/' })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  wss: Server;
   constructor(
     private readonly messagesWsService: MessagesWsService
   ) { }
   handleConnection(client: Socket) {
     this.messagesWsService.register(client);
+    this.wss.emit('clients-updated',this.messagesWsService.getConnectedClients());
   }
   handleDisconnect(client: Socket) {
     this.messagesWsService.remove(client.id);
+    this.wss.emit('clients-updated',this.messagesWsService.getConnectedClients());
   }
 }

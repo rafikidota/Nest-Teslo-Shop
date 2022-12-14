a056c180c024b4c363296c516b7955d1971653fd
rafiki <71207415+rafikidota@users.noreply.github.com>
2022-12-14 02:52:59 -0500

    Register and remove client

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -2,15 +2,16 @@ import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nes
 import { Socket } from 'socket.io';
 import { MessagesWsService } from './messages-ws.service';
 
-@WebSocketGateway({ cors: true })
+@WebSocketGateway({ cors: true})
+// @WebSocketGateway({ cors: true, namespace: '/' })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   constructor(
     private readonly messagesWsService: MessagesWsService
-    ) { }
+  ) { }
   handleConnection(client: Socket) {
-    console.log('Client connected: ', client.id);
+    this.messagesWsService.register(client);
   }
   handleDisconnect(client: Socket) {
-    console.log('Client disconnected: '), client.id;
+    this.messagesWsService.remove(client.id);
   }
 }

dfb52ce2b70fff03220ad51d3e64a2d8ae1282a3
rafiki <71207415+rafikidota@users.noreply.github.com>
2022-12-17 14:18:04 -0500

    Emit message from client
     @SubscribeMessage('message-from-client')

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,8 +1,9 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { NewMessageDto } from './dto/new-message.dto';
 import { MessagesWsService } from './messages-ws.service';
 
-@WebSocketGateway({ cors: true})
+@WebSocketGateway({ cors: true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   wss: Server;
@@ -11,10 +12,31 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
   ) { }
   handleConnection(client: Socket) {
     this.messagesWsService.register(client);
-    this.wss.emit('clients-updated',this.messagesWsService.getConnectedClients());
+    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients());
   }
   handleDisconnect(client: Socket) {
     this.messagesWsService.remove(client.id);
-    this.wss.emit('clients-updated',this.messagesWsService.getConnectedClients());
+    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients());
+  }
+  @SubscribeMessage('message-from-client')
+  onMessageFromClient(client: Socket, payload: NewMessageDto) {
+
+    //Emit only for client
+    // client.emit('message-from-server', {
+    //   fullName: 'Soy yo',
+    //   message: payload.message || 'no-message!!'
+    // });
+
+    //Emit everybody except initial client
+    // client.broadcast.emit('message-from-server', {
+    //   fullName: 'Soy yo',
+    //   message: payload.message || 'no-message!!'
+    // });
+
+    //Emit everybody
+    this.wss.emit('message-from-server', {
+      fullName: 'Soy yo',
+      message: payload.message || 'no-message!!'
+    });
   }
 }
